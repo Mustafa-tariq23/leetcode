ba260dd02256bd28432752a81001b9daf17e3ca3
Mustafa Tariq <mt2032303@gmail.com>
2025-05-23 15:55:35 +0500

    twosome solution

---
+++ twosome/2some.ts
@@ -0,0 +1,17 @@
+function twoSum(nums: number[], target: number): number[] {
+  const map: { [key: number]: number } = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if(map[complement] !== undefined) {
+      return [map[complement], i];
+    }
+    
+    map[nums[i]] = i;
+  }
+
+  return [];
+};
+
+console.log(twoSum([0, 3, 7, 2, 4, 6, 0, 8, 2, 5], 0))
